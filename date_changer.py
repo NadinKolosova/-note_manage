6d186632056f2b10e05a29c95219745a97e52d13
NadinKolosova <nadenkakolosova.shop@mail.ru>
2025-01-17 22:01:35 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,19 @@
+created_date ='15-01-2025'
+ssue_date ='20-01-2025'
+temp_created_date='15-01-2025'
+temp_issue_date ='20-01-2025'
+print (f'{created_date}')
+print(f'{ssue_date}')
+print(temp_created_date[0:5])
+print(temp_issue_date[0:5])
+
+
+
+
+
+
+
+
+
+
+
